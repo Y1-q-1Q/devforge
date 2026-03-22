b96d2ba4b563b98e84413102322b35488147e2bc
tinyfish <2689302660@qq.com>
2026-03-22 12:20:05 +0800

    revert: remove v2 scaffold from v1 repo
    v2 code moved to dedicated repo: github.com/Y1-q-1Q/devforge-v2
    This repo remains as v1 (Node.js MVP) in maintenance mode.

--- scripts/init-db.sql
+++
@@ -1,6 +0,0 @@
--- Initialize DevForge databases
-CREATE DATABASE devforge_knowledge;
-CREATE DATABASE devforge_code;
-CREATE DATABASE devforge_flow;
-CREATE DATABASE devforge_deploy;
-CREATE DATABASE devforge_user;
